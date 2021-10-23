6c6874a84b6f0c2394ca98552b4b20dea7b84a0f
Glav Mraz <xloderunnerx@gmail.com>
2021-10-23 23:05:27 +0300

    DoTween.

---
+++ Assets/Scripts/DIrections/Component/Directions.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map.Directions.Component {
+    public class Directions : MonoBehaviour {
+        [SerializeField] private string apiKey;
+    }
+}
