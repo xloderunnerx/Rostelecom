d9996cdae2fd2adf2b816ad9b6c6d132288f9a96
Glav Mraz <xloderunnerx@gmail.com>
2021-10-23 22:56:17 +0300

    Places.

---
+++ Assets/Scripts/Places/Component/Places.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map.Places.Component {
+    public class Places : MonoBehaviour {
+        [SerializeField] private string apiKey;
+        public void StartSearch(double longitude, double latitude, int radius, List<String> types, Action<OnlineMapsGooglePlacesResult[]> onComplete) {
+
+            var allTypes = "";
+            for(int i = 0; i < types.Count; i++) {
+                if (i == 0) {
+                    allTypes += types[i];
+                    break;
+                }
+                allTypes += "|" + types[i];
+            }
+
+            OnlineMapsGooglePlaces.FindNearby(
+                apiKey,
+                new OnlineMapsGooglePlaces.NearbyParams(
+                    longitude,
+                    latitude,
+                    radius)
+                {
+                    types = allTypes
+                }).OnComplete += (s) => {
+                    onComplete?.Invoke(OnFindComplete(s));
+                };
+        }
+
+        public OnlineMapsGooglePlacesResult[] OnFindComplete(string value) {
+            OnlineMapsGooglePlacesResult[] results = OnlineMapsGooglePlaces.GetResults(value);
+
+            if (results == null) {
+                Debug.Log("Error");
+                Debug.Log(value);
+                return null;
+            }
+
+            return results;
+        }
+    }
+}
